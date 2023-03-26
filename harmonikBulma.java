f5334690687bd4e8112482591856f7526a057eee
ismail <126421146+ismail-aykurt@users.noreply.github.com>
2023-03-26 13:04:08 +0300

    Add files via upload

---
+++ harmonikBulma.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class harmonikBulma {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        int number,toplam=0;
+        double value=0;
+        System.out.print("sayı giriniz:");
+        number=input.nextInt();
+        for(int i=1;i<=number;i++) {
+            value+=(1.0/i);//burda dikkat edilmesi gereken int/int=int bir değer döndürür ancak bir değeri double olarak tanımlarsak double değer döner
+
+
+
+        }
+        System.out.println("Girilen sayının harmonik değeri="+value);
+
+    }
+}
